b7a8ac6172a2bc84f659f50eef45b873be3369fd
derejmi <derejemireyigbe@gmail.com>
2021-01-27 10:56:15 +0000

    update portolio further

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-// import Main from "./components/main";
+import Main from "./components/main";
 import { Link } from "react-router-dom";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="demo-big-content">
@@ -37,10 +37,12 @@ class App extends Component {
               <Link to="/projects">Projects</Link>
             </Navigation>
           </Drawer>
-          <Content>
-            <div className="page-content" />
-            {/* <Main /> */}
-          </Content>
+          <div id="cover">
+            <Content>
+              <div className="page-content" />
+              <Main />
+            </Content>
+          </div>
         </Layout>
       </div>
     );
---
+++ src/components/landingpage.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Grid, Cell } from "react-mdl";
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }} id="main-area">
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://drive.google.com/uc?export=view&id=1oanEgszKTVNeHcm1dZf8pXQY8E6sjr2I"
+              alt="animated-me"
+              className="animated-img"
+            />
+
+            <div className="banner-text">
+              <h1>Deré Jemireyigbe</h1>
+              <h2>Software Engineer</h2>
+              <div className="social-links">
+                {/* Github */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                {/* LinkedIn */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/* Spotify*/}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-spotify" aria-hidden="true" />
+                </a>
+
+                {/* Gmail*/}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-envelope" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
