43a29fccd7a06603d629ee85e9dfd00009821742
derejmi <derejemireyigbe@gmail.com>
2021-01-30 21:47:34 +0000

    add changes

--- src/Index.js
+++ src/Index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "react-mdl/extra/material.css";
 import "react-mdl/extra/material.js";
 import { BrowserRouter } from "react-router-dom";
-
+//
 ReactDOM.render(
   <BrowserRouter>
     <App />
